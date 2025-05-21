a9c8b6ba4a809b17ace65dd24f523c70595392f5
maze272003 <jg.jonatas.au@phinmaed.com>
2025-05-22 00:39:04 +0800

    component sidebar

---
+++ sidebar.php
@@ -0,0 +1,32 @@
+<!-- Sidebar -->
+<div class="sidebar">
+            <div class="sidebar-header">
+                <h3><i class="fas fa-bullseye"></i> Spring Bullbars</h3>
+            </div>
+            <div class="sidebar-menu">
+                <ul>
+                <?php if(isset($user['user_level']) && $user['user_level'] === '1'): ?>
+                        <!-- Admin Menu Links -->
+                        <li><a href="home.php"><i class="fas fa-home"></i> Dashboard</a></li>
+                        <li><a href="users.php"><i class="fas fa-users"></i> Users</a></li>
+                        
+                        <li><a href="product.php"><i class="fas fa-box-open"></i> Products</a></li>
+                        <li><a href="add_product.php"><i class="fa-solid fa-plus"></i> Add Products</a></li>
+                    
+                        <li><a href="sales.php"><i class="fas fa-shopping-cart"></i> Sales</a></li>
+                        <li><a href="sales_report.php"><i class="fa-solid fa-calendar-days"></i>Selacted date Sales</a></li>
+                        <!-- <li><a href="reports.php"><i class="fas fa-chart-bar"></i> Reports</a></li> -->
+                    <?php elseif(isset($user['user_level']) && $user['user_level'] === '2'): ?>
+                        <!-- Special User Menu Links -->
+                        <li><a href="home.php"><i class="fas fa-home"></i> Dashboard</a></li>
+                        <li><a href="product.php"><i class="fas fa-box-open"></i> Products</a></li>
+                        <li><a href="sales.php"><i class="fas fa-shopping-cart"></i> Sales</a></li>
+                    <?php elseif(isset($user['user_level']) && $user['user_level'] === '3'): ?>
+                        <!-- User Menu Links -->
+                        <li><a href="home.php"><i class="fas fa-home"></i> Dashboard</a></li>
+                        <li><a href="product.php"><i class="fas fa-box-open"></i> Products</a></li>
+                    <?php endif; ?>
+                    <li><a href="logout.php"><i class="fas fa-sign-out-alt"></i> Logout</a></li>
+                </ul>
+            </div>
+        </div>
--- users.php
+++ users.php
@@ -301,35 +301,7 @@
 </head>
 <body>
     <div class="admin-container">
-        <!-- Sidebar -->
-        <div class="sidebar">
-            <div class="sidebar-header">
-                <h3><i class="fas fa-bullseye"></i> Spring Bullbars</h3>
-            </div>
-            <div class="sidebar-menu">
-                <ul>
-                    <?php if(isset($user['user_level']) && $user['user_level'] === '1'): ?>
-                        <!-- Admin Menu Links -->
-                        <li><a href="home.php"><i class="fas fa-home"></i> Dashboard</a></li>
-                        <li><a href="users.php" class="active"><i class="fas fa-users"></i> Users</a></li>
-                        <li><a href="categorie.php"><i class="fas fa-tags"></i> Categories</a></li>
-                        <li><a href="product.php"><i class="fas fa-box-open"></i> Products</a></li>
-                        <li><a href="sales.php"><i class="fas fa-shopping-cart"></i> Sales</a></li>
-                        <li><a href="reports.php"><i class="fas fa-chart-bar"></i> Reports</a></li>
-                    <?php elseif(isset($user['user_level']) && $user['user_level'] === '2'): ?>
-                        <!-- Special User Menu Links -->
-                        <li><a href="home.php"><i class="fas fa-home"></i> Dashboard</a></li>
-                        <li><a href="product.php"><i class="fas fa-box-open"></i> Products</a></li>
-                        <li><a href="sales.php"><i class="fas fa-shopping-cart"></i> Sales</a></li>
-                    <?php elseif(isset($user['user_level']) && $user['user_level'] === '3'): ?>
-                        <!-- User Menu Links -->
-                        <li><a href="home.php"><i class="fas fa-home"></i> Dashboard</a></li>
-                        <li><a href="product.php"><i class="fas fa-box-open"></i> Products</a></li>
-                    <?php endif; ?>
-                    <li><a href="logout.php"><i class="fas fa-sign-out-alt"></i> Logout</a></li>
-                </ul>
-            </div>
-        </div>
+    <?php include_once('sidebar.php'); ?>
         
         <!-- Main Content -->
         <div class="main-content">
